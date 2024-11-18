5e86c0a345786df3b7d231edc539ea2397209235
Shouq <shouq000@hotmail.com>
2024-11-18 19:56:37 +0300

    update

--- PersonalWebsite/PersonalWebsite/wsgi.py
+++ PersonalWebsite/PersonalWebsite/wsgi.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 import sys
 import os
 
-sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'PersonalWebsite')))
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'PersonalWebsite.settings')
 
 from django.core.wsgi import get_wsgi_application
