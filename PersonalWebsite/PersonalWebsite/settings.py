697f2fe9080f0c664741fd9aa8b17cab8fc334cc
Shouq <shouq000@hotmail.com>
2024-11-18 18:46:54 +0300

    update

--- PersonalWebsite/PersonalWebsite/settings.py
+++ PersonalWebsite/PersonalWebsite/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -26,7 +27,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG","False").lower()=="true"
 
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -84,7 +85,8 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-database_url= os.environ.get("DATABASE_URL")
+
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
@@ -151,19 +153,3 @@ EMAIL_HOST_PASSWORD = 'your-app-password'  # Use an App Password (not your Gmail
 DEFAULT_FROM_EMAIL = 'your-email@gmail.com'  # Sender email address
 '''
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ["PGDATABASE"],
-        'USER': os.environ["PGUSER"],
-        'PASSWORD': os.environ["PGPASSWORD"],
-        'HOST': os.environ["PRIVATE_DB_HOST"],
-        'PORT': os.environ["PGPORT"],
-    }
-} if not DEBUG else {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
